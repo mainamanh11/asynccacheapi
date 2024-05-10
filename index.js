e2a07b7f5306382834a6858221b7726cee801d42
mainamanh11 <erickg0149pun0suy@hotmail.com>
2024-05-11 05:38:03 +0700

    init project

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function searchInsert(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[mid] < target) left = mid + 1;
-    else right = mid - 1;
+function reverseList(head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
   }
-  return left;
+  return prev;
 }
